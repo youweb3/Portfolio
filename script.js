89ba4a14ed9b211b77b5bac80e013c9bde3b4a42
Kamran <Yousef.shaikhan@gmail.com>
2025-09-28 00:17:09 +0100

    fixed hamburger menu, update style
    Fixed the hamburger menu so that when the user clicks, it will display and can switch to other pages

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// select Element
+// ================== Modal ==================
+document.addEventListener("DOMContentLoaded", () => {
 const modal = document.getElementById("read");
 const readMoreBtn = document.querySelector(".read-more-btn");
 const closeBtn = document.querySelector(".modal-content .close");
 
+if (modal && readMoreBtn && closeBtn) {
 // open modal
 
 readMoreBtn.addEventListener("click", () => {
@@ -22,11 +24,25 @@ window.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
+}
+});
 
 
-// Hamburger menu toggle
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('navLinks');
-  hamburger.onclick = () => {
-    navLinks.classList.toggle('active');
-  };
+// ================== Hamburger Menu ==================
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+
+  //when click outside the menu -> menu should be close
+  document.addEventListener("click", (e) => {
+    if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
+      navLinks.classList.remove("active");
+    }
+  })
+});
